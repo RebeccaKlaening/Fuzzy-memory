275e7f3ed8107c7765330ec0bfa6f0637e04f058
RebeccaKlaening <klaeningrebecca@gmail.com>
2018-11-27 10:55:02 +0100

    added a class of data-image to all my cards, and set them to the same name as the ones that match

--- memory.js
+++ memory.js
@@ -1,14 +1,42 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard(){
-this.classList.toggle('flip');
+this.classList.add('flip');
+
+  if(!hasFlippedCard) {
+      // first click
+    hasFlippedCard = true;
+    firstCard = this;
+}   else {
+    // second click
+  hasFlippedCard = false;
+  secondCard = this;
+
+    // do cards match
+    
+
+  }
 }
 
+
+
+
+
+
+// to shuffle all my cards
  (function shuffle() {
   cards.forEach(card => {
    let ramdomPos = Math.floor(Math.random() * 12);
    card.style.order = ramdomPos;
   });
 })();
+
+
+
+
+
 
 cards.forEach(card=> card.addEventListener('click', flipCard));
