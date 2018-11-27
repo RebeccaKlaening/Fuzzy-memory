f0d6b781df235fd18853a12c0fcd4819ec9c33e2
RebeccaKlaening <klaeningrebecca@gmail.com>
2018-11-27 10:26:00 +0100

    added a function to my script to shuffle all the cards when restart

--- memory.js
+++ memory.js
@@ -3,4 +3,12 @@ const cards = document.querySelectorAll('.memory-card');
 function flipCard(){
 this.classList.toggle('flip');
 }
+
+ (function shuffle() {
+  cards.forEach(card => {
+   let ramdomPos = Math.floor(Math.random() * 12);
+   card.style.order = ramdomPos;
+  });
+})();
+
 cards.forEach(card=> card.addEventListener('click', flipCard));
